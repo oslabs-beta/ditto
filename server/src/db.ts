7272e2d4a0087c1c04e4e65035f8d96063789abd
Jay Cho <jaycho1017@gmail.com>
2024-05-16 12:12:16 -0500

    database connection working and server running

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,18 +1,22 @@
-import { Client } from 'pg';
+import { Pool } from 'pg';
+require('dotenv').config();
 
-const client = new Client({
-	user: process.env.RDS_USERNAME,
+const pool = new Pool({
+	user: process.env.RDS_USERNAME_JAY,
 	host: process.env.RDS_HOST,
 	database: process.env.NODE_ENV === 'test' ? 'test' : 'postgres',
-	password: process.env.RDS_PASSWORD,
+	password: process.env.RDS_PASSWORD_JAY,
 	port: 5432,
+	ssl: {
+		rejectUnauthorized: false,
+	},
 });
 
 const db = {
 	async connect(): Promise<void> {
 		// Connect to the database
 		try {
-			await client.connect();
+			await pool.connect();
 			console.log('Connected to PostgreSQL database');
 		} catch (err: unknown) {
 			if (err instanceof Error) {
@@ -26,7 +30,7 @@ const db = {
 	async end(): Promise<void> {
 		// End connection to database
 		try {
-			await client.end();
+			await pool.end();
 			console.log('Connection to database ended');
 		} catch (err: unknown) {
 			if (err instanceof Error) {
@@ -39,7 +43,7 @@ const db = {
 
 	async query(queryString: string): Promise<any> {
 		try {
-			const result = await client.query(queryString);
+			const result = await pool.query(queryString);
 			return result.rows; // should return rows for SELECT
 		} catch (err: unknown) {
 			if (err instanceof Error) {
