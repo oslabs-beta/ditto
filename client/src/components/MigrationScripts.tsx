d624ab57e8c1b068071105abd31ce964ef1bf38c
syjeffkim <107138664+syjeffkim@users.noreply.github.com>
2024-05-19 23:27:07 -0700

    my user was undefined because i did not import the store to my migrationscript file

--- client/src/components/MigrationScripts.tsx
+++ client/src/components/MigrationScripts.tsx
@@ -1,35 +1,75 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { setMigrationVersions } from '../store';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
+interface Migration {
+	version: string;
+	description: string;
+	date_migrated: string;
+	state: string;
+	execution_time: string;
+}
 
 const MigrationScripts: React.FC = () => {
-	const migrationversions = useSelector(
-		(state: any) => state.migrationversions
-	);
+	const selectedDatabase = useSelector((state: any) => state.selectedDatabase);
+	const username = useSelector((state: any) => state.user); // user
+	console.log('current user:', username);
+	const [migrations, setMigrations] = useState<Migration[]>([]);
+
+	useEffect(() => {
+		const fetchMigrations = async () => {
+			try {
+				const response = await fetch('/databases', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+						// Authorization: `Bearer YOUR_JWT_TOKEN`, // Replace with your JWT token logic
+					},
+					body: JSON.stringify({
+						dbName: selectedDatabase,
+						username: username,
+					}),
+				});
+
+				if (!response.ok) {
+					throw new Error(`HTTP error! status: ${response.status}`);
+				}
+
+				const result = await response.json();
+				console.log('result: ', result);
+				setMigrations(result.migrations);
+			} catch (error) {
+				console.error('Error fetching migrations:', error);
+			}
+		};
+
+		if (selectedDatabase) {
+			fetchMigrations();
+		}
+	}, [selectedDatabase]);
+
 	return (
-		<div className="migrationTableContainer">
-			<div className="title">Migration Table</div>
-			<table className="migrationTable">
-				<thead>
-					<tr>
-						<th>Version</th>
-						<th>Description</th>
-						<th>Date Migrated</th>
-						<th>State</th>
-						<th>Execution Time</th>
+		<table>
+			<thead>
+				<tr>
+					<th>Version</th>
+					<th>Description</th>
+					<th>Date Migrated</th>
+					<th>State</th>
+					<th>Execution Time</th>
+				</tr>
+			</thead>
+			<tbody>
+				{migrations.map((migration, index) => (
+					<tr key={index}>
+						<td>{migration.version}</td>
+						<td>{migration.description}</td>
+						<td>{migration.date_migrated}</td>
+						<td>{migration.state}</td>
+						<td>{migration.execution_time}</td>
 					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>1.0.0</td>
-						<td>Initial Migration</td>
-						<td>2024-05-17</td>
-						<td>Success</td>
-						<td>30s</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
+				))}
+			</tbody>
+		</table>
 	);
 };
 
