0d925cb613bc18d424acfb458f73458771cfad1e
Jay Cho <jaycho1017@gmail.com>
2024-05-21 14:39:00 -0500

    no real changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ import { Route as Route } from 'react-router-dom';
 import { Routes as Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import LoginPage from './pages/LoginPage';
-import favicon from '/client/src/public/favicon.ico';
+import favicon from './public/favicon.ico';
 import logoImage from './assets/logo.png'; // imports logo img, can be used with react components
 import MigrationPage from './pages/MigrationPage';
 import SignUpPage from './pages/SignUpPage';
