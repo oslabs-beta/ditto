6d78f121115e5fdce828c788edcbff7886e9c955
syjeffkim <107138664+syjeffkim@users.noreply.github.com>
2024-05-22 16:45:07 -0700

    add background img for all pages, located in navbar.css

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -34,27 +34,31 @@ const NavBar: React.FC<NavBarProps> = ({ logo }) => {
 
 	console.log(navLinks);
 	return (
-		<nav>
-			<ul style={{ display: 'flex', alignItems: 'center', listStyle: 'none' }}>
-				{logo && (
-					<li>
-						<img src={logo} alt="Logo" style={{ height: '50px' }} />
-					</li>
-				)}
-				{navLinks.map((link, index) => (
-					<li key={index} style={{ marginLeft: '20px' }}>
-						<Link to={link.path}>{link.label}</Link>
-					</li>
-				))}
-				<li style={{ marginLeft: 'auto' }}></li>
-				{isLoggedIn && (
-					<button className="logOut" onClick={handleLogOut}>
-						Log Out
-					</button>
-				)}
-			</ul>
-			{/* <UserBubble /> */}
-		</nav>
+		<div className="navbar">
+			<nav>
+				<ul
+					style={{ display: 'flex', alignItems: 'center', listStyle: 'none' }}
+				>
+					{logo && (
+						<li>
+							<img src={logo} alt="Logo" style={{ height: '50px' }} />
+						</li>
+					)}
+					{navLinks.map((link, index) => (
+						<li key={index} style={{ marginLeft: '20px' }}>
+							<Link to={link.path}>{link.label}</Link>
+						</li>
+					))}
+					<li style={{ marginLeft: 'auto' }}></li>
+					{isLoggedIn && (
+						<button className="logOut" onClick={handleLogOut}>
+							Log Out
+						</button>
+					)}
+				</ul>
+				{/* <UserBubble /> */}
+			</nav>
+		</div>
 	);
 };
 
