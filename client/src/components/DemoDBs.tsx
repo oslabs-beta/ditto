08adfce6b183f17124682d9e9bd15b9911bf4db4
shaanmalkovich <65307797+shaanmalkovich@users.noreply.github.com>
2024-06-02 02:12:52 +0000

    landing page buttons

---
+++ client/src/components/DemoDBs.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const DemoDB: React.FC = (props) => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
---
+++ client/src/components/LandingPageBtns.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import '../styles/LandingPageBtns';
+
+const LandingPageBtns: React.FC = () => {
+    return(
+        <div className="btns">
+            <button className="landingPageBtn">
+                <a href="signup" target="_blank" className="Font">Get Started</a>
+            </button>
+
+            <button className="landingPageBtn">
+                <a href="faq" target="_blank" className="Font">Instructions</a>
+            </button>
+        </div>
+    )
+}
+
+export default LandingPageBtns;
