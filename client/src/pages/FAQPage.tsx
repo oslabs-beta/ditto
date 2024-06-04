f206b4c954eee2546269d21e7a997def0c18d743
shaanmalkovich <smk4577@gmail.com>
2024-06-04 10:51:46 -0400

    .env added

--- client/src/pages/FAQPage.tsx
+++ client/src/pages/FAQPage.tsx
@@ -1,32 +1,28 @@
-import * as React from 'react';
+import { FC } from 'react';
 import '../styles/pages/FAQPage.css';
-import '../../src/styles/Global.css';
+import '../styles/Global.css';
 
-const FAQPage: React.FC = () => {
+const FAQPage: FC = () => {
   return (
-    <div className='FAQBox'>
-      <div className='FAQ'>
-        <div className='faqContainer'>
-          <div className='faqTitle'>
-            <h1>Frequently Asked Questions</h1>
-            </div>
-            <ul className='FAQul'>
-              <li className='FAQli'>
-                <p className='Q'><strong><em>What was the inspiration behind Ditto?</em></strong><br /></p>
-                <p className='A'>
-                  As schema management teams grow, so do potential issues with consistencies. With Ditto, we aim to reduce instances of these conflicts to facilitate database schema migration.
-                  </p>
-                  </li>
-                  <li className='FAQli'>
-                    <p className='Q'><strong><em>What lies ahead for Ditto?</em></strong><br /></p>
-                    <p className='A'>
-                      We plan on implementing features that allow users to upload and manage their SQL scripts with an intuitive GUI that displays a list of available scripts with detailed information about each script. Users will be able to use this to perform actions such as executing and deleting scripts.
-                      </p>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
+    <div className='faq-page'>
+      <div className='faq-container'>
+        <h1 className='faq-title'>Frequently Asked Questions</h1>
+        <ul className='faq-list'>
+          <li className='faq-item'>
+            <p className='question'><strong><em>What was the inspiration behind Ditto?</em></strong><br /></p>
+            <p className='answer'>
+              As schema management teams grow, so do potential issues with consistencies. With Ditto, we aim to reduce instances of these conflicts to facilitate database schema migration.
+            </p>
+          </li>
+          <li className='faq-item'>
+            <p className='question'><strong><em>What lies ahead for Ditto?</em></strong><br /></p>
+            <p className='answer'>
+              We plan on implementing features that allow users to upload and manage their SQL scripts with an intuitive GUI that displays a list of available scripts with detailed information about each script. Users will be able to use this to perform actions such as executing and deleting scripts.
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
